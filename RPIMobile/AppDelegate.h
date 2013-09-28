685e0675ad9cadcf2882bad9da825d41bbfec518
Stephen Silber <stephen@Stephens-MacBook-Pro.local>
2013-09-28 15:41:43 -0400

    Initial Commit

---
+++ RPIMobile/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  RPIMobile
+//
+//  Created by Stephen Silber on 9/28/13.
+//  Copyright (c) 2013 Stephen Silber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ RPIMobileTests/RPIMobileTests.m
@@ -0,0 +1,34 @@
+//
+//  RPIMobileTests.m
+//  RPIMobileTests
+//
+//  Created by Stephen Silber on 9/28/13.
+//  Copyright (c) 2013 Stephen Silber. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RPIMobileTests : XCTestCase
+
+@end
+
+@implementation RPIMobileTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
