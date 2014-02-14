1516b6424553b42e309d0a71092b8b2f3969d801
= <stephensilber@gmail.com>
2014-02-14 02:17:11 -0500

    moved server to api.stephensilber.com to better test on the go

--- RPIMobile/DataSources.h
+++ RPIMobile/DataSources.h
@@ -10,14 +10,9 @@
 #define RPIMobile_DataSources_h
 
 #define kAthleticsFeedUrl @"http://rpiathletics.com/rss.aspx?path="
-#define kAthleticsRosterUrl @"http://localhost:8080/v1/roster/"
-#define kAthleticsScheduleUrl @"http://localhost:8080/v1/schedule/"
-#define kLaundryStatusFeedUrl @"http://localhost:8080/v1/laundry"
-
-//#define kAthleticsRosterUrl @"http://192.168.0.17:8080/v1/roster/"
-//#define kAthleticsScheduleUrl @"http://192.168.0.17:8080/v1/schedule/"
-//#define kLaundryStatusFeedUrl @"http://192.168.0.17:8080/v1/laundry"
-
+#define kAthleticsRosterUrl @"http://api.stephensilber.com/v1/roster/"
+#define kAthleticsScheduleUrl @"http://api.stephensilber.com/v1/schedule/"
+#define kLaundryStatusFeedUrl @"http://api.stephensilber.com/v1/laundry"
 
 #define kWeatherStatusUrl @"http://api.openweathermap.org/data/2.5/weather?q=troy,ny"
 #endif
